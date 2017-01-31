c98d92cf25740726f849674c73cb2ffbee55caf2
jls47 <jls47@users.noreply.github.com>
2017-01-30 22:51:03 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,4 +18,49 @@
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
+$(document).ready(function(){
+var topo = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2NoYWVmMTYiLCJhIjoiY2l5bDE1aHZ5MDAydTJ3bnpoaWZqbWpkcSJ9.ip2r4oOtxQdBmGDcL7IaWA', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'your.mapbox.project.id'
 
+}),
+
+satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2NoYWVmMTYiLCJhIjoiY2l5bDE1aHZ5MDAydTJ3bnpoaWZqbWpkcSJ9.ip2r4oOtxQdBmGDcL7IaWA', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'your.mapbox.project.id'
+}),
+
+streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2NoYWVmMTYiLCJhIjoiY2l5bDE1aHZ5MDAydTJ3bnpoaWZqbWpkcSJ9.ip2r4oOtxQdBmGDcL7IaWA', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'your.mapbox.project.id'
+});
+
+var baseMaps = {
+	"Topographic": topo,
+	"Satellite": satellite,
+	"Streets": streets
+};
+
+var insPoint = L.marker([46.773421, -121.745159]).bindPopup("Inspiration Point"),
+	SVT = L.marker([46.794075, -121.715850]).bindPopup("Stevens-Van Trump Historical Monument"),
+	campMuir = L.marker([46.835563, -121.731734]).bindPopup("Camp Muir"),
+	paradise = L.marker([46.785022, -121.734545]).bindPopup("Paradise, the closest town to Rainier")
+
+var POIs = L.layerGroup([insPoint, SVT, campMuir, paradise]);
+	
+var overlayMaps = {
+	"POIs": POIs
+};
+
+//var mymap = L.map('map-container').setView([46.852886, -121.760374], 13);
+var myMap = L.map('map-container', {
+	center: [46.82, -121.760374],
+	zoom: 12,
+	layers: [satellite, POIs]
+});
+
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+});
+
+	
